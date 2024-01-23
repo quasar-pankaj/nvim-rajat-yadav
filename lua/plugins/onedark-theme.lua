f5efcf2bc45e8ca51d0729e27ebaf5e030e1cfdf
laggedSkapari <es.rajatyadav@gmail.com>
2024-01-23 14:25:21 +0530

    Updated one dark theme.

--- lua/plugins/onedark-theme.lua
+++ lua/plugins/onedark-theme.lua
@@ -6,21 +6,11 @@ return {
 			code_style = {
 				keywords = "bold",
 			},
-			colors = {
-				bright_pink = "#ff78b2",
-				bright_orange = "#ff806c",
-				bright_yellow = "#daca77",
-				pure_white = "#ffffff",
-				bright_blue = "#65dfff",
-			},
-			highlights = {
-				["@string"] = { fg = "#ff806c" },
-				["@function"] = { fg = "#65dfff" },
-				["@function.builtin"] = { fg = "#65dfff" },
-				["@keyword"] = { fg = "#ff78b2" },
-				["@character"] = { fg = "#daca77" },
-        ["@preproc"] = {fg = "#ffa245"},
-			},
+      colors = {
+        purple = "#ff78b2",
+        green = "#ff806c",
+
+      },
 		})
 		require("onedark").load()
 	end,
