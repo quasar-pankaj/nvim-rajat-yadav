ad1ced2333fe5763e84ff19c2535e52491701890
laggedSkapari <es.rajatyadav@gmail.com>
2024-01-23 13:53:41 +0530

    Update my one dark theme.

--- lua/plugins/onedark-theme.lua
+++ lua/plugins/onedark-theme.lua
@@ -1,13 +1,27 @@
 return {
-  "navarasu/onedark.nvim",
-  config = function()
-    require('onedark').setup({
-      style = 'deep',
-      code_style = {
-        keywords = 'bold',
-
-      }
-    })
-    require('onedark').load()
-  end
+	"navarasu/onedark.nvim",
+	config = function()
+		require("onedark").setup({
+			style = "deep",
+			code_style = {
+				keywords = "bold",
+			},
+			colors = {
+				bright_pink = "#ff78b2",
+				bright_orange = "#ff806c",
+				bright_yellow = "#daca77",
+				pure_white = "#ffffff",
+				bright_blue = "#65dfff",
+			},
+			highlights = {
+				["@string"] = { fg = "#ff806c" },
+				["@function"] = { fg = "#65dfff" },
+				["@function.builtin"] = { fg = "#65dfff" },
+				["@keyword"] = { fg = "#ff78b2" },
+				["@character"] = { fg = "#daca77" },
+        ["@preproc"] = {fg = "#ffa245"},
+			},
+		})
+		require("onedark").load()
+	end,
 }
