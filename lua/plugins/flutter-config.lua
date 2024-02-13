0c1efd13e46e82a7f9caa272c2c18e55414f55ef
laggedSkapari <es.rajatyadav@gmail.com>
2024-02-13 14:20:28 +0530

    added flutter keybinds.

--- lua/plugins/flutter-config.lua
+++ lua/plugins/flutter-config.lua
@@ -1,9 +1,17 @@
 return {
-  "akinsho/flutter-tools.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "stevearc/dressing.nvim", -- optional for vim.ui.select
-  },
-  config = true,
+	"akinsho/flutter-tools.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim", -- optional for vim.ui.select
+	},
+	ui = {
+		border = "rounded",
+	},
+	decorations = {
+		statusline = {
+			app_version = true,
+		},
+	},
+	config = true,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>fr', ':FlutterRun<CR>')
+vim.keymap.set('n', '<leader>fd', ':FlutterDevices<CR>')
+vim.keymap.set('n', '<leader>fe', ':FlutterEmulators<CR>')
+vim.keymap.set('n', '<leader>fre', ':FlutterReload<CR>')
+vim.keymap.set('n', '<leader>fR', ':FlutterReload<CR>')
